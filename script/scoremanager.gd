9931ac3def570fe6282bdcd5d266118b6726ff04
Newtonietzsche <baptiste.plassoux2@orange.fr>
2022-06-11 08:32:19 +0200

    balance happy

--- script/scoremanager.gd
+++ script/scoremanager.gd
@@ -6,11 +6,12 @@ var carbone
 var happyness = 7200
 var happy_prct = 100
 var mean_happy = 0
+var moy_happy
 var biodiversite
 var energie_consomation
 var energie_production = 12000
 var base_conso = 10000
-var  cycle = 0
+var cycle = 0
 
 
 
@@ -27,8 +28,8 @@ func _money():
 		mean_happy = mean_happy + happy_prct
 	else:
 		money = money + (10000*mean_happy/minute)
+		moy_happy = round(mean_happy/minute)
 		mean_happy = 0
-		
 func emmission_carbon():
 	pass
 func satisfaction():
@@ -38,11 +39,20 @@ func satisfaction():
 	else :
 		if happyness < 7200:
 			happyness = happyness + 1
-	print(happy_prct)
+	
 func energie_consom():
 	if cycle < minute :
 		cycle = cycle +1
 	else:
+		if moy_happy >= 99:
+			base_conso = base_conso + 10
+		if moy_happy > 80 and mean_happy < 99:
+			base_conso = base_conso + 5
+		if moy_happy<50 and moy_happy > 10:
+			base_conso = base_conso - 5
+		if moy_happy <= 10:
+			base_conso = base_conso - 10
+		moy_happy = 0
 		cycle = 0
 	var random = RandomNumberGenerator.new()
 	random.randomize()
